381b56e20288b3683fb88b9c64f1bd408ad0876b
Restyled.io <commits@restyled.io>
2024-08-17 22:26:20 +0000

    Restyled by yapf

--- src/GeneralStateTestsFiller/Cancun/stEIP1153-transientStorage/basic_tload.py
+++ src/GeneralStateTestsFiller/Cancun/stEIP1153-transientStorage/basic_tload.py
@@ -19,9 +19,7 @@ REFERENCE_SPEC_VERSION = "2f8299df31bb8173618901a03a8366a3183479b0"
 
 
 @pytest.mark.valid_from("Cancun")
-def test_basic_tload(
-    state_test: StateTestFiller,
-):
+def test_basic_tload(state_test: StateTestFiller, ):
     """Covered .json vectors:
     load arbitrary value is 0 at beginning of transaction (01_tloadBeginningTxnFiller.yml)
     tload from same slot after tstore returns correct value (02_tloadAfterTstoreFiller.yml)
@@ -58,41 +56,36 @@ def test_basic_tload(
     tload_from_sstore_result = 18
 
     pre = {
-        address_to: Account(
+        address_to:
+        Account(
             balance=1000000000000000000,
             nonce=0,
             code=Op.JUMPDEST()
             # 01_tloadBeginningTxnFiller.yml
             + Op.SSTORE(tload_at_transaction_begin_result, Op.TLOAD(0))
             # 02_tloadAfterTstoreFiller.yml
-            + Op.TSTORE(2, tstore_value)
-            + Op.SSTORE(tload_after_tstore_result, Op.TLOAD(2))
-            + Op.SSTORE(tload_after_tstore_result_second_time, Op.TLOAD(2))
+            + Op.TSTORE(2, tstore_value) +
+            Op.SSTORE(tload_after_tstore_result, Op.TLOAD(2)) +
+            Op.SSTORE(tload_after_tstore_result_second_time, Op.TLOAD(2))
             # 03_tloadAfterStoreIs0Filler.yml
-            + Op.TSTORE(3, tstore_value)
-            + Op.SSTORE(tload_wrong_after_tstore_result, Op.TLOAD(0))
+            + Op.TSTORE(3, tstore_value) +
+            Op.SSTORE(tload_wrong_after_tstore_result, Op.TLOAD(0))
             # 16_tloadGasFiller.yml calculate tload gas
-            + Op.TSTORE(16, 2)
-            + Op.MSTORE(0, Op.GAS())  # hot load the memory
-            + Op.MSTORE(0, Op.GAS())
-            + Op.TLOAD(16)
-            + Op.MSTORE(32, Op.GAS())
-            + Op.SSTORE(
-                tload_nonzero_gas_price_result, Op.SUB(Op.MLOAD(0), Op.MLOAD(32))
-            )
-            + Op.SSTORE(
-                tload_nonzero_gas_price_result, Op.SUB(Op.SLOAD(16), extra_opcode_gas)
-            )
+            + Op.TSTORE(16, 2) + Op.MSTORE(0, Op.GAS())  # hot load the memory
+            + Op.MSTORE(0, Op.GAS()) + Op.TLOAD(16) + Op.MSTORE(32, Op.GAS()) +
+            Op.SSTORE(tload_nonzero_gas_price_result,
+                      Op.SUB(Op.MLOAD(0), Op.MLOAD(32))) +
+            Op.SSTORE(tload_nonzero_gas_price_result,
+                      Op.SUB(Op.SLOAD(16), extra_opcode_gas))
             # from zero slot
-            + Op.MSTORE(0, Op.GAS())
-            + Op.TLOAD(5)
-            + Op.MSTORE(32, Op.GAS())
-            + Op.SSTORE(tload_zero_gas_price_result, Op.SUB(Op.MLOAD(0), Op.MLOAD(32)))
-            + Op.SSTORE(
-                tload_zero_gas_price_result, Op.SUB(Op.SLOAD(1601), extra_opcode_gas)
-            )
+            + Op.MSTORE(0, Op.GAS()) + Op.TLOAD(5) + Op.MSTORE(32, Op.GAS()) +
+            Op.SSTORE(tload_zero_gas_price_result,
+                      Op.SUB(Op.MLOAD(0), Op.MLOAD(32))) +
+            Op.SSTORE(tload_zero_gas_price_result,
+                      Op.SUB(Op.SLOAD(1601), extra_opcode_gas))
             # 18_tloadAfterStoreFiller.yml
-            + Op.SSTORE(18, 22) + Op.SSTORE(tload_from_sstore_result, Op.TLOAD(18)),
+            + Op.SSTORE(18, 22) +
+            Op.SSTORE(tload_from_sstore_result, Op.TLOAD(18)),
             storage={
                 tload_at_transaction_begin_result: 0xFF,
                 tload_after_tstore_result: 0xFF,
@@ -103,7 +96,8 @@ def test_basic_tload(
                 tload_from_sstore_result: 0xFF,
             },
         ),
-        TestAddress: Account(
+        TestAddress:
+        Account(
             balance=7000000000000000000,
             nonce=0,
             code="0x",
@@ -122,8 +116,7 @@ def test_basic_tload(
             tload_nonzero_gas_price_result: 100,
             tload_zero_gas_price_result: 100,
             tload_from_sstore_result: 0x00,
-        }
-    )
+        })
 
     tx = Transaction(
         nonce=0,
